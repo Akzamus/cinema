3f5fad0a6a7159e5f31b5a9760a83bae9834ba76
Akzam Khamitov <makoron3002@gmail.com>
2024-02-24 14:21:47 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const authRouter = require('./authRouter')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+app.use("/auth", authRouter)
+
+const start = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://mongo:mongo@cluster0.zzy14se.mongodb.net/cinema?retryWrites=true&w=majority", {
+            useNewUrlParser: true, useUnifiedTopology: true
+        });
+        app.listen(PORT, () => console.log(`server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
+
